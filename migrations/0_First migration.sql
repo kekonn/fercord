8138fc0553ac45264675218327165d92b5d8907a
kekkon <ketsukonn@gmail.com>
2023-03-27 16:31:29 +0200

    Change initial migration

--- migrations/0_First migration.sql
+++ migrations/0_First migration.sql
@@ -1,11 +1,12 @@
 -- public.reminders definition
 
 CREATE TABLE public.reminders (
-                                  who numeric NOT NULL,
+                                  who varchar NOT NULL,
                                   "when" timestamptz NOT NULL,
                                   what varchar NOT NULL,
-                                  "server" numeric NOT NULL,
-                                  channel numeric NOT NULL,
+                                  "server" varchar NOT NULL,
+                                  channel varchar NOT NULL,
                                   id bigint NOT NULL GENERATED ALWAYS AS IDENTITY
 );
+
 CREATE INDEX reminders_when_idx ON public.reminders ("when");
