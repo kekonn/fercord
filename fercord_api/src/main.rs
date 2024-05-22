4419229db4cc58422e2627c995f0790e001f9c10
kekkon <ketsukonn@gmail.com>
2024-05-22 21:07:01 +0200

    Convert any error to api error in healthcheck

--- fercord_api/src/main.rs
+++ fercord_api/src/main.rs
@@ -1,16 +1,41 @@
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, Result, web};
 use actix_web::http::header::ContentType;
-use actix_web::{HttpServer, App, web, HttpRequest, Responder, HttpResponse, Result};
+use anyhow::anyhow;
+
 use fercord_storage::config::DiscordConfig;
+use fercord_storage::db::{Pool, setup};
 use fercord_storage::kv::KVClient;
-use fercord_storage::db::{setup, Pool};
 
 use crate::model::HealthCheck;
 
 mod model;
 
+#[derive(Debug)]
+struct ApiError {
+    err: anyhow::Error
+}
+
+impl Display for ApiError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "ApiError: {}", self.err)
+    }
+}
+
+impl actix_web::error::ResponseError for ApiError {
+}
+
+impl From<anyhow::Error> for ApiError {
+    fn from(err: anyhow::Error) -> Self {
+        ApiError { err }
+    }
+}
+
 async fn health_check(_req: HttpRequest, db: web::Data<Pool>, kv: web::Data<KVClient>) -> Result<impl Responder> {
-    let db = db.acquire().await?;
-    kv.connection_check().await?;
+    db.acquire().await.map_err(|err| ApiError::from(anyhow!(err)))?;
+    kv.connection_check().await.map_err(ApiError::from)?;
     
     Ok(HttpResponse::Ok()
         .content_type(ContentType::json())
